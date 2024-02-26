b0d61ab7fc73357c17bb681c7a4638a57eec7f96
inspy <inspiredscout@gmail.com>
2024-02-26 20:29:28 +0300

    added checkOrder api renamed api-start file (its still useless)

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express"
 import { create_order } from "../create_order";
 import { test } from "../test";
 import { test2 } from "../test2";
+import { checkOrder } from "../checkOrder";
 
 const router = express.Router();
 
@@ -18,5 +19,10 @@ router.get('/api/test', (req: Request, res: Response) => {
 router.get('/api/test2', (req: Request, res: Response) => {
     // Обработка запроса для test
     test2(req, res);
+});
+
+router.get('/api/checkOrder', (req: Request, res: Response) => {
+    // Обработка запроса для test
+    checkOrder(req, res);
 });
 export default router;
---
+++ src/checkOrder.ts
@@ -0,0 +1,25 @@
+import { db } from './db'
+import { Router, Request, Response } from 'express';
+import routes from './api/routes';
+
+export async function checkOrder(req: Request, res: Response) {
+    const Order = req.body
+    const findOrder = await db?.order.findMany({
+        where: {
+            OR: [
+            {id : Order.id},
+            {customerName : Order.customerName},
+            {customerEmail : Order.customerEmail},
+            {customerPhone : Order.customerPhone},
+            {customerAdress : Order.customerAdress}
+            ],
+        },
+        include: {
+            orderItems: true
+        }
+})
+    if (findOrder.length === 0) return res.json({message:'Такого заказа нету', success: false})
+    else {
+        return res.json({success: true, findOrder})
+    }
+}
