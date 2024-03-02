ae010d9635ac38480e4e0de8a5ab124d2f976d80
Sh1rosura <nik.gololobov91@mail.ru>
2024-03-02 16:28:54 +0300

    i am gay

--- src/assets/pages/catalog/Catalog.tsx
+++ src/assets/pages/catalog/Catalog.tsx
@@ -6,8 +6,8 @@ function Catalog() {
 
             <header>
                 <nav className={styles.navbar}>
+                    <img src='./logo.svg' alt=''/>
                     <div className={styles.conteiner}>
-                        <img src='./logo.svg' alt=''/>
                         <ul>
                             <Link to='/'>
                                 <li>Home</li>
@@ -17,10 +17,139 @@ function Catalog() {
                 </nav>
             </header>
 
-            <div className={styles.info}>
-                <p>Гриша когда работать будешь????????</p>
+            <div className={styles.catalog}>
+                <div className={styles.filters}>
+
+                    <ul className={styles.types}>
+                        {/*<li>Oils and liquids</li>
+                        <li>Accessories</li>
+                        <li>Tires and rims</li>
+                        <li>Product for maintenance</li>*/}
+                        <li>Мама Вани</li>
+                        <li>Папа вани</li>
+                        <li>Кот вани</li>
+                        <li>Брат вани</li>
+                    </ul>
+
+                    <ul className={styles.brands}>
+                        <li><h1>Brands</h1></li>
+                        <li>GigaPremium</li>
+                        <li>GigaGold</li>
+                        <li>GigaPlatinum</li>
+                        <li>GigaSilver</li>
+                    </ul>
+                </div>
+
+                <div className={styles.cards}>
+                    <div className={styles.position}>
+                        <Link to='/Catalog/info'>
+                            <div className={styles.image}>
+                                <img src='./1.jpg' alt=''/>
+                            </div>
+                        </Link>
+
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.position}>
+                        <div className={styles.image}>
+                            <img src='./1.jpg' alt=''/>
+                        </div>
+                        <div className={styles.info}>
+                            <h2>Name</h2>
+                            <p>Price</p>
+                            <button>Add</button>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
+            <footer className={styles.footer}>
+
+            </footer>
         </main>
     )
 }
