46faaf8c7a9ef93ef3e9568d466867495f9134f8
sirok1 <s1r0k.dev@gmail.com>
2024-03-03 03:24:53 +0300

    feat(dev): listen on another port if it is already in use & different .env path

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,26 @@
 import express from "express"
-const bodyParser = require('body-parser');
+import http from "http"
 import routes from "./api/routes"
 import * as dotenv from "dotenv"
-dotenv.config()
+
+dotenv.config({path: "../.env"})
 
 const app = express();
-const port = process.env.API_PORT || 3000;
+let port = Number(process.env.API_PORT) || 3000;
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const main = () => {
+    app.listen(port)
+        .on("listening", () => {
+            console.log(`Server is running on port ${port}`);
+        })
+        .on("error", () => {
+            console.error(`Port ${port} is already in use trying another one...`)
+            port++
+            main()
+        })
+}
+
+main()
