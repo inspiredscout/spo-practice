6e83d785753b9dda6e9dc12095820c5813446b71
sirok1 <s1r0k.dev@gmail.com>
2024-03-05 02:41:20 +0300

    fix(back-end): swagger.json handle

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import routes from "./api/routes"
 import swaggerUi from "swagger-ui-express"
 import * as dotenv from "dotenv"
-import express, {
-    Response as ExResponse,
-    Request as ExRequest,
-    NextFunction,
-  } from "express";
+import express from "express";
+import swaggerJson from "../docs/swagger.json"
+
 dotenv.config()
 
 
@@ -14,16 +12,9 @@ const port = process.env.API_PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
-app.use(express.static("../docs"))
 app.use("/docs",
     swaggerUi.serve,
-    swaggerUi.setup(undefined,{
-        swaggerOptions: {
-            url: "/docs/swagger.json"
-        }
-    }))
-
-app.use(express.static("../docs"))
+    swaggerUi.setup(swaggerJson))
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
