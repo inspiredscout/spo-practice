36d0c28cfd378008517038af80382ef0cf3134ca
inspy <inspiredscout@gmail.com>
2024-03-04 21:46:47 +0300

    Папа помоги modified:   backend/package.json modified:   backend/pnpm-lock.yaml modified:   backend/src/server.ts new file:   backend/src/swagger.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express"
-const bodyParser = require('body-parser');
+import swaggerUi from "swagger-ui-express"
 import routes from "./api/routes"
 import * as dotenv from "dotenv"
 dotenv.config()
@@ -9,6 +9,14 @@ const port = process.env.API_PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
+app.use("/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(undefined,{
+        swaggerOptions: {
+            url: "/swagger.json"
+        }
+    }))
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
