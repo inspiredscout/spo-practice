12e51ff051aa61436c4f741bd8ac62cec93cfcc2
sirok1 <s1r0k.dev@gmail.com>
2024-03-07 15:47:41 +0300

    fix(master): routing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,13 +12,13 @@ let port = Number(process.env.API_PORT) || 3000;
 
 app.use(express.json());
 app.use(routes);
-app.use(`${process.env.DOCS_PATH}`,swaggerUi.serve,swaggerUi.setup(swaggerJson))
+app.use(`${apiPrefix}${process.env.DOCS_PATH}`,swaggerUi.serve,swaggerUi.setup(swaggerJson))
 
 const main = () => {
     app.listen(port)
         .on("listening", () => {
             console.log(`Server is running on port ${port}`);
-            console.log(`Swagger UI is available at http://0.0.0.0:${port}${process.env.DOCS_PATH}`);
+            console.log(`Swagger UI is available at http://0.0.0.0:${port}${apiPrefix}${process.env.DOCS_PATH}`);
         })
         .on("error", () => {
             console.error(`Port ${port} is already in use trying another one...`)
