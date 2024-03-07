636144ef75402aac7e4c7df2e781941a74084140
sirok1 <s1r0k.dev@gmail.com>
2024-03-07 11:50:37 +0300

    fix(dev): use dynamic api prefix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ let port = Number(process.env.API_PORT) || 3000;
 
 app.use(express.json());
 app.use(routes);
-app.use("/api/docs",swaggerUi.serve,swaggerUi.setup(swaggerJson))
+app.use(`${apiPrefix}/docs`,swaggerUi.serve,swaggerUi.setup(swaggerJson))
 
 const main = () => {
     app.listen(port)
