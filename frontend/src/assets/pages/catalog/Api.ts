0a103f860e65d9d75694a91d40b948391aef8bd1
Sh1rosura <nik.gololobov91@mail.ru>
2024-03-09 16:11:38 +0300

    [1] Закомментил тестовую апи

--- frontend/src/assets/pages/catalog/Api.ts
+++ frontend/src/assets/pages/catalog/Api.ts
@@ -1,11 +1,11 @@
-import {Card} from './CatalogData.ts'
-
-export const getCardList = (): Promise<Card[]> =>
-     fetch('https://randomuser.me/api/')
-        .then(async (result) => {
-            if (result.ok) {
-                return (await result.json()) as Card[]
-            } else {
-                throw new Error(result.statusText)
-            }
-        });
+// import {Card} from './CatalogData.ts'
+//
+// export const getCardList = (): Promise<Card[]> =>
+//      fetch('https://randomuser.me/api/')
+//         .then(async (result) => {
+//             if (result.ok) {
+//                 return (await result.json()) as Card[]
+//             } else {
+//                 throw new Error(result.statusText)
+//             }
+//         });
