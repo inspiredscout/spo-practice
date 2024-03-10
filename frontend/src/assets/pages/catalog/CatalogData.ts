d1476abe9fc1976b8b0e18c342fab0ed71467b8c
Sh1rosura <nik.gololobov91@mail.ru>
2024-03-10 22:42:38 +0300

    Бля я чет понажимал и такое случилось

--- frontend/src/assets/pages/catalog/CatalogData.ts
+++ frontend/src/assets/pages/catalog/CatalogData.ts
@@ -1,12 +1,34 @@
 import axios from 'axios';
 
-export const fetchData = async () => {
+export interface Photo {
+    id: number;
+    photoId: number;
+    url: string;
+}
+
+export interface Product{
+    id: number;
+    name: string;
+    price: string;
+    description: string;
+    type: string;
+    country: string;
+    color: string;
+    brand: string;
+    visibility: boolean;
+    photos: Photo[];
+}
+
+
+const fetchData = async (): Promise<Product[]> => {
     try {
-        const response = await axios.get('https://randomuser.me/api/');
-        return response.data.results; // Возвращаем результаты из ответа API (предположим, что данные находятся в массиве 'results')
+        const response = await axios.get('https://spo.ultrapivomode.space/api/product/getAll');
+        const products: Product[] = response.data; // Типизация данных как массив продуктов
+        return products; // Возвращаем данные для дальнейшего использования
     } catch (error) {
-        console.error('Ошибка при выполнении запроса:', error);
-        throw error;
+        console.error('Ошибка при получении данных:', error);
+        return []; // Возвращаем пустой массив в случае ошибки
     }
 };
 
+export { fetchData };
