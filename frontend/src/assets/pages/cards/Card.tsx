eeba2da36e4caba0b109ac3133ed1562bb2f16c0
Вася Пупкин <125453763+Sh1rosura@users.noreply.github.com>
2024-03-13 01:46:09 +0300

    Update Card.tsx

--- frontend/src/assets/pages/cards/Card.tsx
+++ frontend/src/assets/pages/cards/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {Link, useParams} from 'react-router-dom';
 import axios from 'axios';
 import styles from './Cards.module.css';
